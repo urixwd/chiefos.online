6f2299630a47a1942cd8dc4e20bf7c3e7ea4f660
urixwd <urixwd@gmail.com>
2025-02-19 17:03:22 +0100

    Sections titles

--- src/types/stage.ts
+++ src/types/stage.ts
@@ -35,7 +35,7 @@ Check-In
   {
     id: "stage-1",
     title:
-      "Ensures a flawless experience from pre-embarkation to five-star reviews at",
+      "Ensures a flawless experience from pre-embarkation to five-star reviews",
     boldText: "Check-Cut",
     imageUrl:
       "https://f003.backblazeb2.com/file/chiefos-website/screenshots/screenshot-1.png",
@@ -43,7 +43,7 @@ Check-In
   {
     id: "stage-2",
     title:
-      "Empower your guests to manage what they can, and deliver your expertise exactly when it's needed.",
+      "Empower your guests to manage what they can, and deliver your expertise exactly when it's needed",
     boldText: "Troubleshooting",
     imageUrl:
       "https://f003.backblazeb2.com/file/chiefos-website/screenshots/screenshot-1.png",
@@ -51,7 +51,7 @@ Check-In
   {
     id: "stage-3",
     title:
-      "Proactively notifies to ensure safety and prevent damages by having Chief.OS watching your guests back.",
+      "Proactively notifies to ensure safety and prevent damages by having ChiefOS watching your guests back",
     boldText: "Boat Operation",
     imageUrl:
       "https://f003.backblazeb2.com/file/chiefos-website/screenshots/screenshot-1.png",
@@ -66,7 +66,7 @@ Check-In
   },
   {
     id: "stage-5",
-    title: "Get your guests ready well ahead embarkation.",
+    title: "Get your guests ready well ahead embarkation",
     boldText: "Check-In",
     imageUrl:
       "https://f003.backblazeb2.com/file/chiefos-website/screenshots/screenshot-1.png",
